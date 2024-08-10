c980527f0cffa7d689f8e0e2eba3ee846accdbba
HongQuan78 <vhquan313@gmail.com>
2024-08-10 22:59:00 +0700

    add cors

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 require('dotenv').config(); // Load environment variables from .env file
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
 
+// Configure CORS options
+const corsOptions = {
+    origin: function (origin, callback) {
+        // List of allowed origins
+        const allowedOrigins = ['http://localhost:3000', 'https://crud.live'];
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true); // Allow CORS for this request
+        } else {
+            callback(new Error('Not allowed by CORS')); // Block the request
+        }
+    },
+    optionsSuccessStatus: 200 // For legacy browser support
+};
+
+app.use(cors(corsOptions)); // Enable CORS with the specified options
 app.use(express.json());
 
 // MongoDB connection
@@ -32,12 +48,14 @@ app.post('/api/hash', (req, res) => {
         .catch(err => res.status(400).send(err));
 });
 
+// Route to get a single hash by classId
 app.get('/api/hash/:classId', (req, res) => {
     Hash.findOne({ classId: req.params.classId })
         .then(hash => res.send(hash))
         .catch(err => res.status(400).send(err));
 });
 
+// Route to get all hashes
 app.get('/api/hash', (req, res) => {
     Hash.find()
         .then(hash => res.send(hash))
